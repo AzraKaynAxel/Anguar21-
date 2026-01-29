a88d51044d567d293a716791e7706826c9ca34c8
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-01-29 10:09:01 +0100

    Feature/routes (#2)
    * Module et component requis pour notre app
    
    * Céation des routes
    
    ---------
    
    Co-authored-by: AzraKayn/Axel <axforjob.hay@gmail>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+/**
+ import { Routes } from '@angular/router';
+ export const routes: Routes = [];
+ */
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import {Home} from './site/home/home';
+import {About} from './site/about/about';
+import {Films} from './site/films/films';
+import {Errors} from './site/errors/errors';
+
+
+/**
+ * Configuration des Routes
+ * Le Mapping interne de notre application Angular
+ * Dans un tableau JSON de type Routes
+ *
+ */
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'films', component: Films },
+  { path: '404', component: Errors },
+  { path: '**', redirectTo: '/404'}
+];
+
