a05180e54cfac8d429ebe9eedc999427b8cc4b5e
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-01-30 15:42:43 +0100

    Feature/service (#10)
    * MAJ ReadME
    
    * Mise en place de la navbar
    
    * MAJ ReadME
    
    * MAJ ReadME
    
    * Mise en place de services pour le formulaire de validation
    
    ---------
    
    Co-authored-by: AzraKayn/Axel <axforjob.hay@gmail>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+/** Import qui va permettre de gérer différente requête */
+import { provideHttpClient} from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+
+    // Instanciation de la méthode
+    provideHttpClient()
   ]
 };
